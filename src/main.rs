0806f057ac9b2cf3d0c95a17660d8ac975427506
Erik Funder Carstensen <funder@fastmail.com>
2022-01-07 15:44:22 +0100

    Update to be system agnostic, and change interface

--- src/main.rs
+++ src/main.rs
@@ -1,23 +1,17 @@
-use std::env::args_os;
-use std::fs::File;
-use std::os::windows::prelude::*;
+use std::fs::OpenOptions;
+
+const FILE_NAME: &str = "BIG";
+
+const GI_B: u64 = 1024 * 1024 * 1024;
 
 fn main() -> anyhow::Result<()> {
-    let input = args_os()
-        .skip(1)
-        .next()
-        .expect("Expected a number describing the size of the wanted file in GiBs!");
-
-    let buffer = File::create("BIG")?;
-    buffer.seek_write(
-        &[0],
-        input
-            .to_str()
-            .expect("Expected valid utf-8 in the first argument")
-            .parse::<u64>()?
-            * 1024
-            * 1024
-            * 1024,
-    )?;
+    let buffer = OpenOptions::new()
+        .create(true)
+        .write(true)
+        .open(FILE_NAME)
+        .unwrap();
+    let size = buffer.metadata().unwrap().len();
+    let new_size = GI_B.max(size * 2);
+    buffer.set_len(new_size).unwrap();
     Ok(())
 }
